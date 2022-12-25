d3fb4bc2dc17a948665365792453bc47de6db015
anirudh242 <anirudhbhardwaj48@gmail.com>
2022-12-25 19:32:03 +0530

    Hello, Triangle

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -o out -I ./src ./src/*.c -lglfw -lGL -lX11 -lpthread -lXrandr -ldl ; ./out
+	g++ -o out -I ./src ./src/*.c -lglfw -lGL -lGLEW -lX11 -lpthread -lXrandr -ldl ; ./out
--- src/main.c
+++ src/main.c
@@ -1,16 +1,31 @@
-#include "glad.h"
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdbool.h>
 
+// GLSL Shader source code
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
 // Function declarations
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
 int main(int argc, char *argv[])
 {
+    //glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
     glfwInit();
-
-    // Configuring GLFu
+    // Configuring GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -24,18 +39,81 @@ int main(int argc, char *argv[])
     }
 
     glfwMakeContextCurrent(window); 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+        printf("Error initizializing GLEW");
+
+
+
+    // Vertex shader
+    float vertices[] = { // Vertices of the triangle
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };  
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Checking for succesful compilation
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION FAILED\n");
+        printf("%s", infoLog);
+    }
+
+    // Fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // Attaching the shaders
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     // Gameloop
     while(!glfwWindowShouldClose(window))
     {
         // Input
-        // processInput(window);
+        processInput(window);
 
         // Rendering
-        gladLoadGL(); // glClearColor() and glClear() result in segfaults without this
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Check and call events and swap buffers
         glfwSwapBuffers(window);
